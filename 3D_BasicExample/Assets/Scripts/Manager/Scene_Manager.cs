419b793ca42f4eff1a4b689d705b3b48c39c09a0
HiKeng <46863998+HiKeng@users.noreply.github.com>
2022-03-22 23:22:36 +0700

    Added restart scene on dead.

---
+++ 3D_BasicExample/Assets/Scripts/Manager/Scene_Manager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            _RestartScene();
+        }
+    }
+
+    public void _RestartSceneWithDelay(float _delayTime)
+    {
+        StartCoroutine(_StartRestartSceneWithDelay(_delayTime));
+    }
+
+    IEnumerator _StartRestartSceneWithDelay(float WaitTime)
+    {
+        yield return new WaitForSeconds(WaitTime);
+
+        _RestartScene();
+    }
+
+    public void _RestartScene()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+}
